6e616942f2ec322ac2cf4698e21c99c61b9eb1ed
kot_eugene <to_ev@mail.ru>
2023-10-03 14:06:18 +0300

    n out of m tests passes added

--- src/c_tests.c
+++ src/c_tests.c
@@ -2,18 +2,28 @@
 
 c_tests_vector global_c_tests;
 
+static int handle_test(c_tests_node *test) {
+    int error;
+    c_tests_error_message message = {.error = TEST_SUCCESS, .error_message = ""};
+    test->test_func(&message);
+    error = message.error;
+    if (error == TEST_SUCCESS)
+        printf("[ PASS ] %s\n", test->name);
+    else if (error == TEST_ERROR)
+        printf("[ FAIL ] %s: %s\n", test->name, message.error_message);
+    return error;
+}
+
 int run_all_c_tests() {
+    size_t success_amount = 0;
     for (size_t i = 0; i < global_c_tests.size; i++) {
-        c_tests_error_message message = {.error = TEST_SUCCESS, .error_message = ""};
-        global_c_tests.tests[i].test_func(&message);
-        if (message.error == TEST_SUCCESS) {
-            printf("[ PASS ] %s\n", global_c_tests.tests[i].name);
-        } else if (message.error == TEST_ERROR) {
-            printf("[ FAIL ] %s: %s\n", global_c_tests.tests[i].name, message.error_message);
-        }
+        int error;
+        error = handle_test(&global_c_tests.tests[i]);
+        if (error == TEST_SUCCESS) success_amount++;
     }
+    printf("\n%zu out of %zu tests passed\n", success_amount, global_c_tests.size);
     c_tests_vector_clear(&global_c_tests);
-    return 0;
+    return success_amount;
 }
 
 void global_c_tests_init() { c_tests_vector_init(&global_c_tests); }
